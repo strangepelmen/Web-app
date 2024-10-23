482a547b30526e7fce1e1ca76b5b831f437bc88f
Eugene <139226682+strangepelmen@users.noreply.github.com>
2024-10-23 21:26:22 +0300

    few test changes

---
+++ mainscript.js
@@ -0,0 +1,7 @@
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
+
+tg.expand(); //расширяем на все окно  
+
+bg_color = tg.ThemeParams.bg_color;
+text_color = tg.ThemeParams.text_color;
+hint_color = tg.ThemeParams.hint_color;
